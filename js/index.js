a6aa8770c3dc7453f90fbe9c895f83a70882ff8c
Sergio <sergio.lorca.dev@gmail.com>
2020-01-10 19:02:34 +0100

    remove invalid code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
  // when a message is received from the page code
 window.onmessage = (event) => {
     if (event.data) {
-        console.log('data rec ', event.data);
         loadGrid(event.data);
     }
 };
@@ -53,47 +52,3 @@ function loadGrid(imageElements) {
     });
     $('.lds-roller').addClass('hide-element');
 }
-//add grid elements
-/*document.addEventListener('DOMContentLoaded', function (event) {
-    let emptySquare = {
-        width: 8,
-        height: 8,
-        imageUrl: '',
-        isEmpty: true
-    };
-    const gridElements = [];//Should get from db
-    const imageElements = [
-        {
-            width: 100,
-            height: 100,
-            offsetTop: 10,
-            offsetLeft: 10,
-            imageUrl: 'https://dummyimage.com/100x100/red/0011ff&text=customer+image',
-            linkUrl: 'http://google.es',
-            isEmpty: false
-        }
-    ];
-
-    // this for only to fill 10000 empty squares
-    for (let row = 0; row < 100; row++) {
-        for (let column = 0; column < 100; column++) {
-            emptySquare.offsetLeft = column * 10;
-            emptySquare.offsetTop = row * 10;
-            if (!isFilled(emptySquare, imageElements)){
-                gridElements.push(_.clone(emptySquare));
-            }
-        }
-    }
-
-    const gridContainer = $('.grid-container');
-    const resultGrid = gridElements.concat(imageElements);
-    resultGrid.forEach(element => {
-        if (element.isEmpty) {
-            $('<div class="empty-square" style="width: ' + element.width + 'px; height: ' + element.height + 'px; top: ' + element.offsetTop + 'px; left: ' + element.offsetLeft + 'px;"><div>').appendTo(gridContainer);
-        } else {
-            const imageElement = '<a href="' + element.linkUrl + '" target="_blank"><img style="" src="' + element.imageUrl + '"></a>'
-            $('<div class="image-container" style="width: ' + element.width + 'px; height: ' + element.height + 'px; top: ' + element.offsetTop + 'px; left: ' + element.offsetLeft + 'px;">' + imageElement + '<div>').appendTo(gridContainer);
-        }
-    });
-    $('.lds-roller').addClass('hide-element');
-});*/

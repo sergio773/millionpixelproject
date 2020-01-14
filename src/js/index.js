c089ce41092c901f5b0c3f4585c09d28d2ecb283
Sergio Lorca <sergio.lorca.dev@gmail.com>
2020-01-14 22:58:03 +0100

    Reservation system

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,24 @@ require('../css/modal.css');
 require('../css/timer.css');
 require('./utils');
 import { reservePixels, startReservation } from './reservation'
+
 // when a message is received from the page code
 window.onmessage = (event) => {
+    switch (event.data.action) {
+        case 'loadGridElements':
+            loadGrid(event.data.imageElements);
+            break;
+        case 'add-element':
+            const element = event.data.gridElement;
+            global.reservedElement = element;
+            const gridContainer = $('.grid-container');
+            addReservedElement(element, gridContainer);
+            removeSquaresOnElement(element);
+            break;
+        default:
+            break;
+    }
+
     if (event.data) {
         loadGrid(event.data);
     }
@@ -13,10 +29,12 @@ window.onmessage = (event) => {
 
 // global variables
 global.reservationEnabled = false;
+// TODO: store reserved element on session storage
+global.reservedElement = undefined;
 
 // golbal functions
 global.seeOurCharities = function () {
-    window.parent.postMessage('see-charities', '*');
+    window.parent.postMessage({ action: 'see-charities' }, '*');
 }
 
 global.reservePixels = function () {
@@ -27,8 +45,24 @@ global.startReservation = function () {
     startReservation();
 }
 
+global.cancelreservation = function () {
+    $('.timer-reserve').addClass('hide-element');
+    if (global.reservedElement) {
+        addSquaresOnElement(global.reservedElement,  $('.grid-container'));
+        $(`#${global.reservedElement._id}`).remove();
+        window.parent.postMessage({ action: 'cancel-reservation', id: global.reservedElement._id }, '*');
+    }
+    global.reservedElement = undefined;
+}
+
 function isFilled(square, imageElements) {
+    if ( !imageElements || imageElements.length === 0) {
+        return false;
+    }
     return imageElements.some(element => {
+        if (element.isReservation && isReservedElementExpired(element)) {
+            return false;
+        }
         const offSetLeftLast = element.width + element.offsetLeft;
         const offSetTopLast = element.height + element.offsetTop;
         const isInsideX = square.offsetLeft >= element.offsetLeft && square.offsetLeft < offSetLeftLast;
@@ -51,7 +85,7 @@ function loadGrid(imageElements) {
         for (let column = 0; column < 100; column++) {
             emptySquare.offsetLeft = column * 10;
             emptySquare.offsetTop = row * 10;
-            if (!isFilled(emptySquare, imageElements)){
+            if (!isFilled(emptySquare, imageElements)) {
                 gridElements.push(_.clone(emptySquare));
             }
         }
@@ -60,12 +94,67 @@ function loadGrid(imageElements) {
     const gridContainer = $('.grid-container');
     const resultGrid = gridElements.concat(imageElements);
     resultGrid.forEach(element => {
-        if (element.isEmpty) {
-            $('<div class="empty-square" style="width: ' + element.width + 'px; height: ' + element.height + 'px; top: ' + element.offsetTop + 'px; left: ' + element.offsetLeft + 'px;"><div>').appendTo(gridContainer);
+        if (element.isEmpty && !element.isReservation) {
+            addEmptySquare(element, gridContainer);
+        } else if (element.isReservation) {
+            addReservedElement(element, gridContainer);
         } else {
             const imageElement = '<a href="' + element.linkUrl + '" target="_blank"><img style="" src="' + element.imageUrl + '"></a>'
             $('<div class="image-container" style="width: ' + element.width + 'px; height: ' + element.height + 'px; top: ' + element.offsetTop + 'px; left: ' + element.offsetLeft + 'px;">' + imageElement + '<div>').appendTo(gridContainer);
         }
     });
     $('.lds-roller').addClass('hide-element');
+}
+
+function addReservedElement(element, container) {
+    if (isReservedElementExpired(element)) {
+        return;
+    }
+    $(`<div id="${element._id}" class="reserved" style="width: ${element.width}px; height: ${element.height}px; top: ${element.offsetTop}px; left: ${element.offsetLeft}px;"><div>`).appendTo(container);
+}
+
+function addEmptySquare(element, container) {
+    $(`<div class="empty-square ${element.offsetLeft}-${element.offsetTop}" style="width: ${element.width}px; height: ${element.height}px; top: ${element.offsetTop}px; left: ${element.offsetLeft}px;"><div>`).appendTo(container);
+}
+
+function isReservedElementExpired(element) {
+    const currentTimeSeconds = new Date().getTime() / 1000;
+    const maxReservationSeconds = 10 * 60;
+    if (element.createdAt && (currentTimeSeconds - (new Date(element.createdAt).getTime() / 1000)) > maxReservationSeconds) {
+        return true;
+    }
+    return false;
+}
+
+function removeSquaresOnElement(element) {
+    let x = element.offsetLeft;
+    let y = element.offsetTop;
+    while (y < (element.offsetTop + element.height)) {
+        while (x < (element.offsetLeft + element.width)) {
+            $(`.empty-square.${x}-${y}`).remove();
+            x = x + 10;
+        }
+        y = y + 10;
+        x = element.offsetLeft;
+    }
+}
+
+function addSquaresOnElement(element, container) {
+    let x = element.offsetLeft;
+    let y = element.offsetTop;
+    while (y < (element.offsetTop + element.height)) {
+        while (x < (element.offsetLeft + element.width)) {
+            const emptySquare = {
+                width: 8,
+                height: 8,
+                offsetTop: y,
+                offsetLeft: x,
+            };
+
+            addEmptySquare(emptySquare, $('.grid-container'));
+            x = x + 10;
+        }
+        y = y + 10;
+        x = element.offsetLeft;
+    }
 }
--- src/js/reservation.js
+++ src/js/reservation.js
@@ -17,7 +17,8 @@ export function reservePixels() {
             offsetLeft: Number(selectedElements[0].style.left.split('px')[0]),
             imageUrl: '',
             linkUrl: '',
-            isEmpty: false
+            isEmpty: false,
+            isReservation: true
         };
 
         let modalTop = elementToPurchase.height + elementToPurchase.offsetTop;
@@ -35,22 +36,29 @@ export function reservePixels() {
 
 export function startReservation() {
     if (elementToPurchase) {
-        console.log('element to reserve ', elementToPurchase);
+        window.parent.postMessage({ action: 'reserve', elementToPurchase }, '*');
+        // TODO: validate captcha
         $('.modal-reserve').addClass('hide-element');
         $('.timer-reserve').removeClass('hide-element');
         const timer = $('.time');
         let startTime = 10*60; // 10 min
-        setInterval(()=> {
+        const timerInterval = setInterval(()=> {
+            if (startTime === 0) {
+                clearInterval(timerInterval);
+                // TODO: notify to user the time is over, hide timmer and free the squares
+                return;
+            }
             --startTime;
             var minutes = Math.floor(startTime / 60);
             var seconds = startTime - minutes * 60;
-            timer.html(minutes + ':' + seconds);
+            timer.html(str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2));
         }, 1000);
-        // TODO: format left 0 on seconds
-        // TODO: stop interval on 0 and set as free reserved squares
-        // TODO: validate captcha
-        // TODO: set element as reserved and block for other users
     } else {
         console.error('undefined elementToPurchase');
     }
 }
+
+function str_pad_left(string,pad,length) {
+    return (new Array(length+1).join(pad)+string).slice(-length);
+}
+
